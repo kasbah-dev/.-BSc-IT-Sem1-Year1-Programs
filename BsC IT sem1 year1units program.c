e0e616c5e55ba552bdb8f0153f1fa5d3dabccd22
kasbah-dev <Mihkasbah@gmail.com>
2026-09-11 17:42:55 +0300

    Add files via upload

---
+++ BsC IT sem1 year1units program.c
@@ -0,0 +1,23 @@
+// program to display BsC Information and Technology units for sem1 year1
+/*
+Name:Brian M
+Reg No:CT100/G/30716/26
+Description:BsC Information Technology sem1 year1 units program 
+Date:Friday 11 September 
+Version 1
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("Bachelor of Information Technology Year1 Sem1 Units\n");
+    printf("1.Introduction to programming and algorithms \n");
+    printf("2.Fundamental of digital technology and artificial intelligence\n");
+    printf ("3.Introduction to computer application\n");
+    printf ("4.Essentials of accounting \n");
+    printf ("5.Computer system and organization\n");
+    printf ("6.Analogue electronics\n");
+    printf ("7.Mathematics for science\n");
+    printf ("8.Communication and information literacy skills\n");
+    return 0;
+}
